8cd582da5580c6656b968868c3196ed396ad3186
Kn99HN <khanhnguyen99hn@gmail.com>
2020-05-26 08:59:34 -0400

    update schema

--- server/db.js
+++ server/db.js
@@ -1,4 +1,40 @@
 const mongoose = require('mongoose')
 
 const Profile = new mongoose.Schema({
+    name: String,
+    type: String,
+    position: String,
+    description: String,
+    links: {
+        github: String,
+        facebook: String,
+        linkedin: String,
+        website: String
+    }
 })
+
+const Event = new mongoose.Schema({
+    name: String,
+    date: Date,
+    location: {
+        building: String,
+        room: String
+    },
+    link: {
+        slides: String,
+        recording: String
+    }
+})
+
+const Project = new mongoose.Schema({
+    name: String,
+    link: {
+        github: String,
+        website: String
+    },
+    mentors: String //change this later,
+})
+
+module.exports = mongoose.model('Profile', Profile)
+module.exports = mongoose.model('Event', Event)
+module.exports = mongoose.model('Project', Project)
