6683d724e86ca6d51439e756d3f1a507a3f7bf7a
Kn99HN <khanhnguyen99hn@gmail.com>
2020-05-27 11:59:31 -0400

    finishing firebase setup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
-const config = require('./utils/config')
-const port = config.PORT
+const port = 3001
+const db = require('./db')
 
 const app = express()
 app.use(express.json())
@@ -15,29 +15,64 @@ app.get('/', (req, res) => {
 
 
 app.get('/profiles', (req, res) => {
-    
-})
+    db.ref('profiles/').on('value', (snapshot) => {
+        const profiles = snapshot.val()
+        res.json(profiles)
+    })
+})  
 
 app.get('/projects', (req, res) => {
-
+    db.ref('projects/').on('value', (snapshot) => {
+        const projects = snapshot.val()
+        res.json(projects)
+    })
 })
 
 app.get('/events', (req, res) => {
-
+    db.ref('events/').on('value', (snapshot) => {
+        const events = snapshot.val()
+        res.json(events)
+    })
 })
 
 
 app.post('/profiles/:profile', (req, res) => {
-
+    const profile = req.body
+    const eboard = {
+        name: profile.name,
+        position: profile.position,
+        description: profile.description,
+        links: {
+            github: profile.github ? profile.github : "",
+            facebook: profile.facebook ? profile.facebook : "",
+            email: profile.email ? profile.email : "",
+            linkedin: profile.linkedin ? profile.linkedin : ""
+        }
+    }
+    db.ref(`profiles/${profile.uid}`).set(eboard)
 })
 
 //add user auth here later
 app.post('/events/:event', (req, res) => {
-
+    const event = req.body
+    const Event = {
+        name: event.name,
+        date: event.date,
+        location: event.location,
+        description: event.description,
+        slides: event.slides ? event.slides : "No slides for this event"
+    }
+    db.ref(`events/${event.uid}`).set(Event)
 })
 
 app.post('/projects/:project', (req, res) => {
-
+    const project = req.body
+    const Project = {
+        name: project.name,
+        description: project.description,
+        github: project.github
+    }
+    db.ref(`projects/${project.uid}`).set(Project)
 })
 
 app.listen(port, (req, res) => {
--- server/utils/config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-
-let PORT = process.env.PORT
-let MONGODB_URI = process.env.MONGODB_URI
-
-module.exports = {
-    MONGODB_URI,
-    PORT
-}
