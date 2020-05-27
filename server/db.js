57aa33324b5528f163db09e9f8d0e1ccd26717aa
Kn99HN <khanhnguyen99hn@gmail.com>
2020-05-26 22:52:38 -0400

    refactoring into firebase

--- server/db.js
+++ server/db.js
@@ -1,60 +1,78 @@
-const mongoose = require('mongoose')
-const config = require('./utils/config')
+// const mongoose = require('mongoose')
+// const config = require('./utils/config')
 
-const Profile = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    type: String,
-    position: String,
-    description: {
-        type: String,
-        minlength: 10,
-        required: true
-    },
-    links: {
-        github: String,
-        facebook: String,
-        linkedin: String,
-        website: String
-    }
-})
+// const Profile = new mongoose.Schema({
+//     name: {
+//         type: String,
+//         required: true,
+//     },
+//     type: String,
+//     position: String,
+//     description: {
+//         type: String,
+//         minlength: 10,
+//         required: true
+//     },
+//     links: {
+//         github: String,
+//         facebook: String,
+//         linkedin: String,
+//         website: String
+//     }
+// })
 
-const Event = new mongoose.Schema({
-    name: {
-        type: String,
-        minlength: 5,
-        required: true
-    },
-    date: {
-        type: Date,
-        required: true
-    },
-    location: {
-        building: String,
-        room: String
-    },
-    link: {
-        slides: String,
-        recording: String
-    }
-})
+// const Event = new mongoose.Schema({
+//     name: {
+//         type: String,
+//         minlength: 5,
+//         required: true
+//     },
+//     date: {
+//         type: Date,
+//         required: true
+//     },
+//     location: {
+//         building: String,
+//         room: String
+//     },
+//     link: {
+//         slides: String,
+//         recording: String
+//     }
+// })
 
-const Project = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    link: {
-        github: String,
-        website: String
-    },
-    mentors: String //change this later,
-})
+// const Project = new mongoose.Schema({
+//     name: {
+//         type: String,
+//         required: true
+//     },
+//     link: {
+//         github: String,
+//         website: String
+//     },
+//     mentors: String //change this later,
+// })
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+// mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
-module.exports = mongoose.model('Profile', Profile)
-module.exports = mongoose.model('Event', Event)
-module.exports = mongoose.model('Project', Project)
+// module.exports = mongoose.model('Profile', Profile)
+// module.exports = mongoose.model('Event', Event)
+// module.exports = mongoose.model('Project', Project)
+
+const firebase = require('firebase')
+const config = {
+    apiKey: jsonContent.apiKey,
+    authDomain: jsonContent.authDomain,
+    databaseURL: jsonContent.databaseURL,
+    projectId: jsonContent.projectId,
+    storageBucket: jsonContent.storageBucket,
+    messagingSenderId: jsonContent.messagingSenderId,
+    appId: jsonContent.appId,
+    measurementId: jsonContent.measurementId
+}
+
+firebase.initializeApp(config);
+
+const db = firebase.database()
+
+module.exports = db
--- server/index.js
+++ server/index.js
@@ -1,18 +1,13 @@
 const express = require('express')
 const cors = require('cors')
 const config = require('./utils/config')
-const mongoose = require('mongoose')
-require('./db')
-const Event = mongoose.model('Event')
-const Profile = mongoose.model('Profile')
-const Project = mongoose.model('Project')
 const port = config.PORT
 
 const app = express()
 app.use(express.json())
 app.use(cors())
   
-
+//@ToDo: closing mongo connection
 app.get('/', (req, res) => {
     return res.send("<h1> Hello world </h1>")
 })
@@ -20,52 +15,29 @@ app.get('/', (req, res) => {
 
 
 app.get('/profiles', (req, res) => {
-    Profile.find({}).then(profiles => {
-        res.json(profiles.map(profile => profile.toJSON()))
-    })
+    
 })
 
 app.get('/projects', (req, res) => {
-    Project.find({}).then(projects => {
-        res.json(projects.map(projects => projects.toJSON()))
-        // mongoose.connection.close()
-    })
+
 })
 
 app.get('/events', (req, res) => {
-    Event.find({}).then(events => {
-        res.json(events.map(event => event.toJSON()))
-        // mongoose.connection.close()
-    })
+
 })
 
 
 app.post('/profiles/:profile', (req, res) => {
-    const profile = new Profile(req.body)
-    profile.save()
-    .then(savedProfile => {return savedProfile.toJSON()})
-    .then(formattedProfile => {
-        res.json(formattedProfile)
-    })
+
 })
 
 //add user auth here later
 app.post('/events/:event', (req, res) => {
-    const event = new Event(req.body)
-    event.save()
-    .then(savedEvent => {return savedEvent.toJSON()})
-    .then(formattedEvent => {
-        res.json(formattedEvent)
-    })
+
 })
 
 app.post('/projects/:project', (req, res) => {
-    const project = new Project(req.body)
-    project.save()
-    .then(savedProject => {return savedProject.toJSON()})
-    .then(formattedProject => {
-        res.json(formattedProject)
-    })
+
 })
 
 app.listen(port, (req, res) => {
