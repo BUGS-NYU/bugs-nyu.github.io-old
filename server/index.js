f4e78a37a35cbd3dc455055a58e4716b553dc32f
Kn99HN <khanhnguyen99hn@gmail.com>
2020-05-27 12:24:25 -0400

    adding frontend

--- server/index.js
+++ server/index.js
@@ -1,81 +1,18 @@
+require('dotenv').config()
+const PORT = process.env.PORT
 const express = require('express')
-const cors = require('cors')
-const port = 3001
-const db = require('./db')
-
 const app = express()
-app.use(express.json())
+const apiRouter = require('./router/api')
+const cors = require('cors')
+
 app.use(cors())
-  
-//@ToDo: closing mongo connection
+app.use('/api', apiRouter)
+
 app.get('/', (req, res) => {
-    return res.send("<h1> Hello world </h1>")
+    // res.sendFile(__dirname + 'index')
+    res.send('<h1> Hello world </h1>')
 })
 
-
-
-app.get('/profiles', (req, res) => {
-    db.ref('profiles/').on('value', (snapshot) => {
-        const profiles = snapshot.val()
-        res.json(profiles)
-    })
-})  
-
-app.get('/projects', (req, res) => {
-    db.ref('projects/').on('value', (snapshot) => {
-        const projects = snapshot.val()
-        res.json(projects)
-    })
+app.listen(PORT, (req, res) => {
+    console.log('Listening on port ' + PORT)
 })
-
-app.get('/events', (req, res) => {
-    db.ref('events/').on('value', (snapshot) => {
-        const events = snapshot.val()
-        res.json(events)
-    })
-})
-
-
-app.post('/profiles/:profile', (req, res) => {
-    const profile = req.body
-    const eboard = {
-        name: profile.name,
-        position: profile.position,
-        description: profile.description,
-        links: {
-            github: profile.github ? profile.github : "",
-            facebook: profile.facebook ? profile.facebook : "",
-            email: profile.email ? profile.email : "",
-            linkedin: profile.linkedin ? profile.linkedin : ""
-        }
-    }
-    db.ref(`profiles/${profile.uid}`).set(eboard)
-})
-
-//add user auth here later
-app.post('/events/:event', (req, res) => {
-    const event = req.body
-    const Event = {
-        name: event.name,
-        date: event.date,
-        location: event.location,
-        description: event.description,
-        slides: event.slides ? event.slides : "No slides for this event"
-    }
-    db.ref(`events/${event.uid}`).set(Event)
-})
-
-app.post('/projects/:project', (req, res) => {
-    const project = req.body
-    const Project = {
-        name: project.name,
-        description: project.description,
-        github: project.github
-    }
-    db.ref(`projects/${project.uid}`).set(Project)
-})
-
-app.listen(port, (req, res) => {
-    console.log('Listening on port ' + port)
-})
-
---
+++ server/router/api.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../db')
+
+  
+//@ToDo: closing firebase connection
+router.get('/', (req, res) => {
+    return res.send("<h1> Hello world </h1>")
+})
+
+
+// Adding error handling
+router.get('/profiles', (req, res) => {
+    db.ref('profiles/').on('value', (snapshot) => {
+        const profiles = snapshot.val()
+        res.json(profiles)
+    })
+})  
+
+router.get('/projects', (req, res) => {
+    db.ref('projects/').on('value', (snapshot) => {
+        const projects = snapshot.val()
+        res.json(projects)
+    })
+})
+
+router.get('/events', (req, res) => {
+    db.ref('events/').on('value', (snapshot) => {
+        const events = snapshot.val()
+        res.json(events)
+    })
+})
+
+
+router.post('/profiles/:profile', (req, res) => {
+    const profile = req.body
+    const eboard = {
+        name: profile.name,
+        position: profile.position,
+        description: profile.description,
+        links: {
+            github: profile.github ? profile.github : "",
+            facebook: profile.facebook ? profile.facebook : "",
+            email: profile.email ? profile.email : "",
+            linkedin: profile.linkedin ? profile.linkedin : ""
+        }
+    }
+    db.ref(`profiles/${profile.uid}`).set(eboard)
+})
+
+//add user auth here later
+router.post('/events/:event', (req, res) => {
+    const event = req.body
+    const Event = {
+        name: event.name,
+        date: event.date,
+        location: event.location,
+        description: event.description,
+        slides: event.slides ? event.slides : "No slides for this event"
+    }
+    db.ref(`events/${event.uid}`).set(Event)
+})
+
+router.post('/projects/:project', (req, res) => {
+    const project = req.body
+    const Project = {
+        name: project.name,
+        description: project.description,
+        github: project.github
+    }
+    db.ref(`projects/${project.uid}`).set(Project)
+})
+
+
+module.exports = router
