7e0aa7829907e34d304e5e1fb4795a45f05aff0c
Kn99HN <khanhnguyen99hn@gmail.com>
2020-05-29 15:35:41 -0400

    update homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,83 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import Team from './components/Team';
-import Events from './components/Events'
-import {BrowserRouter as Router, Switch, Link, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Link, Route, NavLink} from 'react-router-dom'
 import TeamPage from './pages/TeamPage';
 import HomePage from './pages/HomePage';
 import EventsPage from './pages/EventsPage';
+import styled from 'styled-components';
+import bugslogo from './img/bugslogo.jpg';
+import githublogo from './img/GitHub-Mark-32px.png';
+
+const Wrapper = styled.section`
+  padding: 1.5em;
+  background: white;
+`;
+
+const NavBar = styled.header`
+  background-color: white;
+`
+
+const NavList = styled.ul`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: row wrap;
+  justify-content: space-between;
+`
+
+const Links = styled.nav`
+  padding-right: 0.5em;
+  padding-left: 0.5em;
+`
+
+const Header = styled.div`
+  padding-right: 0.5em;
+  padding-left: 0.5em;
+  margin-right: auto;
+`
+
+const Footer = styled.footer`
+  padding: 1.5em;
+  background-color: lightgray;
+`
 
 const App = () => {
   return(
     <Router>
+      <div className="App">
+        <Wrapper>
+          <NavBar>
+            <NavList>
+              <Header>
+                <Link to="/"> 
+                  {/* <Logo src = {bugslogo} /> */}
+                  BUGS
+                 </Link>
+              </Header>
+              <Links>
+                <NavLink to = "/team"> Team </NavLink>
+              </Links>
+              <Links>
+                <NavLink to = "/events"> Events </NavLink>
+              </Links>
+            </NavList>
+          </NavBar>
+        </Wrapper>
       <Switch>
         <Route exact path = "/" component = {HomePage}></Route>
         <Route path = "/team" component = {TeamPage}></Route>
         <Route exact path = "/events" component = {EventsPage}></Route>
       </Switch>
+      <Footer>
+          <NavList>
+              <Header>
+                NYU BUGS, Courant Institute of Mathematical Sciences
+              </Header>
+              <Links>
+                  bugsnyu@gmail.com
+              </Links>
+          </NavList>
+      </Footer>
+      </div>
     </Router>
   )
 }
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import styled from 'styled-components'
+import styled from 'styled-components';
+import background from '../img/background.jpg';
 
 const Title = styled.h1`
   font-size: 2.5em;
@@ -8,46 +9,80 @@ const Title = styled.h1`
 `;
 
 const Wrapper = styled.section`
-  padding: 4em;
-  background: black;
+  background: white;
+  margin: 50px;
 `;
 
+const TextContainer = styled.div`
+  margin: auto;
+  width: 80%;
+  padding: 50px;
+`
+
+const Content = styled.p`
+  text-align: center;
+`
+
+const Image = styled.img`
+  height: auto;
+  width: 100%;
+`
+
+const ImageContainer = styled.div`
+  overflow: hidden;
+`
+
+const LeftContainer = styled.div`
+  float: left;
+`
+
+const RightContainer = styled.div`
+  float: right;
+`
 
 const HomePage = () => {
   const [profiles, setProfile] = useState([])
   const [events, setEvent] = useState([])
 
   return (
-   <div>
-     <Wrapper>
-      <Title>
-        BUGS@NYU - The Open Source Club at NYU
-      </Title>
-     </Wrapper>
-     <h2> About Us </h2>
-     <div>
-      NYU BUGS is the open source club at NYU. 
-      We promote open source by engaging students 
-      through collaborative projects, hosting industry professions,
-      and running open source workshops. 
-     </div>
-     <h2> What's Open Source? </h2>
-     <div>
-        Open source represents a form of licensing that 
-        encourages collaboration and transparency. 
-        Although open source licenses may take a number 
-        of forms, essential features include being able to 
-        freely study, change, and distribute the source code. 
-     </div>
-     <h2> Mozilla PartnerShip </h2>
-     <div>
-        BUGS is a member of the Open 
-        Source Student Network, a network of 
-        university clubs and students that share 
-        the belief that open source software is the 
-        engine that powers innovation. 
-     </div>
-   </div>
+    <main>
+      <ImageContainer>
+    <Image src = {background} />
+  </ImageContainer>
+   <Wrapper>
+        <TextContainer>
+          <Content>
+            <h2> About Us </h2>
+            NYU BUGS is the open source club at NYU. 
+            We promote open source by engaging students 
+            through collaborative projects, hosting industry professions,
+            and running open source workshops. BUGS is a member of the Open 
+            Source Student Network, a network of university clubs and students that share 
+            the belief that open source software is the 
+            engine that powers innovation. 
+          </Content>
+        </TextContainer>
+        <TextContainer>
+          <Content>
+            <h2> What's Open Source? </h2>
+            Open source represents a form of licensing that 
+            encourages collaboration and transparency. 
+            Although open source licenses may take a number 
+            of forms, essential features include being able to 
+            freely study, change, and distribute the source code. 
+          </Content>
+        </TextContainer>
+        <p />
+        <TextContainer>
+           <LeftContainer>
+             <Content> Hello world 1</Content>
+           </LeftContainer>
+           <RightContainer>
+             <Content> Hello world 2 </Content>
+           </RightContainer>
+        </TextContainer>
+   </Wrapper>
+    </main>
   );
 }
 
