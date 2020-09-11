10a30df7603ce1ffd86d048581d3e0af85aea3e5
Evan Silverman <es4753@nyu.edu>
2020-09-10 20:33:43 -0400

    Clean up repository and optimize images to be next-gen formats

---
+++ src/utils/global.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle, keyframes } from "styled-components";
+
+const gradient = keyframes`
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+  }
+`;
+
+export const GlobalStyles = createGlobalStyle`
+  body {
+    background: ${({ theme }) => theme.body};
+    color: ${({ theme }) => theme.text};
+    display: grid;
+    min-height: 100vh;
+    background-size: 400% 400%;
+    animation: ${gradient} 25s ease infinite;
+    overflow-x: hidden;
+  }`;
