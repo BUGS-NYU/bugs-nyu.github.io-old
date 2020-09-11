3449fb5c734b095d29fe45b61f658ab4315783d6
Evan Silverman <es4753@nyu.edu>
2020-09-10 20:44:44 -0400

    fix undo albert's changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-import HomePage from "./HomePage";
-import ProjectsPage from "./ProjectsPage";
-import TeamPage from "./TeamPage";
-import NavBar from "./NavBar";
-import Footer from "./Footer";
-import AlumniPage from "./AlumniPage";
-import ContributorsPage from "./ContributorsPage";
-import EventPage from "./EventPage";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import { createGlobalStyle, keyframes, ThemeProvider } from "styled-components";
-import { lightTheme, darkTheme } from "./theme";
-import { useDarkMode } from "./useDarkMode";
-=======
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 
@@ -29,31 +15,18 @@ import Footer from "./components/Footer";
 import { lightTheme, darkTheme } from "./utils/theme";
 import { GlobalStyles } from "./utils/global";
 import { useDarkMode } from "./utils/useDarkMode";
->>>>>>> 2d5aac7... Clean up repository and optimize images to be next-gen formats
 
-const gradient = keyframes`
-  0% {
-    background-position: 0% 50%;
-  }
-  50% {
-    background-position: 100% 50%;
-  }
-  100% {
-    background-position: 0% 50%;
-  }
-  }
-`
-
-export const GlobalStyles = createGlobalStyle`
-  body {
-    background: ${({ theme }) => theme.body};
-    color: ${({ theme }) => theme.text};
-    display: grid;
-    min-height: 100vh;
-    background-size: 400% 400%;
-    animation: ${gradient} 25s ease infinite;
-    overflow-x: hidden;
-  }`
+// const gradient = keyframes`
+//   0% {
+//     background-position: 0% 50%;
+//   }
+//   50% {
+//     background-position: 100% 50%;
+//   }
+//   100% {
+//     background-position: 0% 50%;
+//   }
+// `;
 
 const App = () => {
   const [theme, toggleTheme, componentMounted] = useDarkMode();
--- src/pages/TeamPage.js
+++ src/pages/TeamPage.js
@@ -28,99 +28,6 @@ const scalein = keyframes`
   }
 `;
 
-const EboardDescriptionContainer = styled.div`
-  width: 100%;
-  white-space: pre-wrap;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  padding: 5px 20px 20px 20px;
-`;
-
-const EboardDescription = styled.p`
-  text-align: left;
-  font-weight: 500;
-  font-style: normal;
-  letter-spacing: 0em;
-  text-transform: none;
-  line-height: 1.2;
-  font-size: calc(0vw + 1rem);
-  color: white;
-  text-align: center;
-  animation: ${scalein} 1s;
-`;
-
-const EboardTitle = styled.h2`
-  font-weight: 300;
-  font-style: normal;
-  letter-spacing: 0em;
-  text-transform: none;
-  text-align: center;
-  line-height: 1;
-  margin-bottom: 0;
-  font-size: calc(0vw + 1.5rem);
-  color: white;
-  animation: ${scalein} 0.9s;
-`;
-
-const EboardContainer = styled.div`
-  margin: 1vw;
-  grid-column: 1
-  min-height: 40vh;
-  height: 100%;
-  background-color: none;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 5px 60px 0px 60px;
-  animation: ${fadein} 1.5s;
-  @media screen and (max-width: 768px) {
-    padding: 0px 0px 0px 0px;
-    margin: 0 0 25px 0;
-    width: 70vw;
-  }
-`;
-
-const EboardSubTitle = styled.p`
-  font-family: var(--normal-font);
-  text-align: center;
-  font-weight: lighter;
-  color: white;
-  margin: 10px 0 0 0;
-  animation: ${scalein} 0.9s;
-  @media screen and (max-width: 768px) {
-    font-size: 1.25rem;
-  }
-`;
-
-const EboardImage = styled.img`
-  width: 70%;
-  object-fit: contain;
-  animation: ${fadein} 1.5s;
-  @media screen and (max-width: 768px) {
-    width: 50%;
-  }
-`;
-
-const Eboard = (props) => {
-  const { name, role, image, description } = props;
-
-  return (
-    <EboardContainer>
-      <EboardImage src={image} />
-
-      <EboardTitle>
-        {name}
-        <EboardSubTitle>{role}</EboardSubTitle>
-      </EboardTitle>
-
-      <EboardDescriptionContainer>
-        <EboardDescription>{description}</EboardDescription>
-      </EboardDescriptionContainer>
-    </EboardContainer>
-  );
-};
-
 const MainContainer = styled.div`
   min-height: 100vh;
   width: 100vw;
@@ -153,7 +60,7 @@ const EboardTitleContainer = styled.div`
   }
 `;
 
-const TeamTitle = styled.h1`
+const EboardTitle = styled.h1`
   font-weight: 300;
   font-style: normal;
   letter-spacing: 0em;
@@ -227,13 +134,12 @@ const TeamPage = () => {
     <MainContainer>
       <PageContainer>
         <EboardTitleContainer>
-          <TeamTitle>
+          <EboardTitle>
             Meet Our <BoldText>Executive Board</BoldText>
-          </TeamTitle>
+          </EboardTitle>
         </EboardTitleContainer>
-
         <MainDescriptionContainer>
-          {eboard.map(({ name, role, description, image }) => {
+          {Object.entries(eboards).map(([name, values]) => {
             return (
               <Eboard
                 name={name}
