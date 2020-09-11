0815bfffe3c120bccfa7c1b58902cc187360b27f
Evan Silverman <es4753@nyu.edu>
2020-09-10 21:01:32 -0400

    Modify data format for team page

--- src/pages/TeamPage.js
+++ src/pages/TeamPage.js
@@ -79,55 +79,63 @@ const BoldText = styled.strong`
   color: white;
 `;
 
-const eboards = {
-  "Albert Liu": {
-    image: [aliu, aliuWebP],
+const eboardData = [
+  {
+    name: "Albert Liu",
+    images: [aliu, aliuWebP],
     role: "President",
     description:
       "Albert majors in Computer Science at CAS. In the past, he's worked on Schedge, an API for NYU course data, as well as HumanToHuman, an application to help researchers study human interaction patterns. In his free time he enjoys going on long walks and taking longer naps.",
   },
-  "Samuel Lin": {
-    image: [slin, slinWebP],
+  {
+    name: "Samuel Lin",
+    images: [slin, slinWebP],
     role: "Vice President",
     description:
       "Samuel is a Senior at CAS majoring in Computer Science and minoring in Web Design and Game Design. He was born and raised in Taiwan, but studied in Singapore for high school before coming to New York. He is a big foodie and enjoys trying foods from different cultures.",
   },
-  "Edmond Shao": {
-    image: [eshao, eshaoWebP],
+  {
+    name: "Edmond Shao",
+    images: [eshao, eshaoWebP],
     role: "Webmaster",
     description:
       "Edmond is a Senior at CAS studying Computer Science and minoring in Business Studies. Aside from class, he likes cars, being a foodie and traveling.",
   },
-  "Eric He": {
-    image: [eriche, ericheWebP],
+  {
+    name: "Eric He",
+    images: [eriche, ericheWebP],
     role: "Secretary",
     description:
       "Eric is a Senior studying Computer Science and Mathematics. He likes to play basketball, listens to music and read about philosophy.",
   },
-  "Khanh Nguyen": {
-    image: [khanhng, khanhngWebP],
+  {
+    name: "Khanh Nguyen",
+    images: [khanhng, khanhngWebP],
     role: "Director of Communications",
     description:
       "Khanh is a Junior from Vietnam studying Computer Science. BROCKHAMPTON is his all-time favorite boyband. Outside of school, find him at Hanoi House",
   },
-  "Ami Nachiappan": {
-    image: [ana, anaWebP],
+  {
+    name: "Ami Nachiappan",
+    images: [ana, anaWebP],
     role: "Event Coordinator",
     description:
       "Ami is a Senior from California studying Computer Science and minoring in Web Design. In her free time, she loves spending time with family, running, and of course, coding!",
   },
-  "Aaditya Mehta": {
-    image: [amehta, amehtaWebP],
+  {
+    name: "Aaditya Mehta",
+    images: [amehta, amehtaWebP],
     role: "Treasurer",
     description:
       "Aaditya is a Senior studying Economics and Mathematics, Aaditya spends way too much time in Stern (#IAmStern). In his free time he likes to listen to music, take long walks, and read Econ papers, sometimes simultaneously.",
   },
-  "Jiayue(Joanna) Zhang": {
+  {
+    name: "Jiayue(Joanna) Zhang",
     role: "Director of Marketing",
     description:
       "Jiayue (Joanna) Zhang is a Senior majoring in Mathematics and Computer Science in CAS. Intellectual challenges of her school work bring her either joy or existential crises; in most cases math the latter and CS the former. One thing she loves about NYC is that performances of her favorite artists are usually only a subway ride away from school. Her guilty pleasure is taking mahjong breaks at Bobst.",
   },
-};
+];
 
 const TeamPage = () => {
   return (
@@ -139,17 +147,15 @@ const TeamPage = () => {
           </EboardTitle>
         </EboardTitleContainer>
         <MainDescriptionContainer>
-          {Object.entries(eboards).map(([name, values]) => {
-            return (
-              <Eboard
-                name={name}
-                role={values.role}
-                description={values.description}
-                images={values.image}
-                key={name}
-              />
-            );
-          })}
+          {eboardData.map(({ name, role, description, images }) => (
+            <Eboard
+              name={name}
+              role={role}
+              description={description}
+              images={images}
+              key={name}
+            />
+          ))}
         </MainDescriptionContainer>
       </PageContainer>
     </MainContainer>
