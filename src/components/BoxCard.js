20c066c5bc1c24613f9d59d2fee1bcadb6f68d38
Teo Nys <teonys@Teos-MacBook-Pro.local>
2021-02-22 21:27:05 -0500

    Box Card styling for project

---
+++ src/components/BoxCard.js
@@ -0,0 +1,120 @@
+import React from "react";
+import styled from "styled-components";
+import githublogo from "../svgs/github.svg";
+
+const BoxCard = ({ title, subtitle, description, link, urlLink }) => {
+  return (
+    <Card>
+      <h1> {title}</h1>
+      <p id={`subtitle`}>{subtitle}</p>
+      <p id={`description`}>{description}</p>
+      <ButtonWrapper>
+        <button id={`projectButton`}>
+          <a href={urlLink}>View Project</a>
+        </button>
+        <button id={`githubButton`}>
+          <GitHubLogo></GitHubLogo>
+          <a href={link}>Github</a>
+        </button>
+      </ButtonWrapper>
+    </Card>
+  );
+};
+const ButtonWrapper = styled.div`
+  display: inline-block;
+
+  #projectButton {
+    width: 150px;
+    height: 40px;
+    background: #ffffff;
+    opacity: 0.8;
+    border: none;
+    border-radius: 5px;
+
+    :hover {
+      opacity: 0.5;
+      cursor: pointer;
+    }
+
+    a {
+      text-decoration: none;
+      height: 100%;
+      width: 100%;
+    }
+  }
+
+  #githubButton {
+    position: relative;
+    margin-left: 10px;
+    width: 150px;
+    height: 40px;
+    background: #9148c9;
+    border-radius: 5px;
+    border: none;
+
+    a {
+      margin-left: 20px;
+      color: #ffffff;
+      text-decoration: none;
+      height: 100%;
+      width: 100%;
+    }
+
+    :hover {
+      opacity: 0.8;
+      cursor: pointer;
+    }
+  }
+`;
+const Card = styled.div`
+  grid-column: auto/span 6;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+  background: linear-gradient(
+    180deg,
+    rgba(255, 255, 255, 0.58) 0%,
+    rgba(226, 226, 226, 0.56) 100%
+  );
+  box-shadow: 0px 4.49006px 22.4503px 11.2251px rgba(0, 0, 0, 0.1);
+  border-radius: 22.4503px;
+  color: #ffffff;
+  padding: 32px;
+
+  h1 {
+    font-weight: bold;
+    font-size: 36px;
+    line-height: 20px;
+    color: #ffffff;
+    margin-block-start: 0.1em;
+    margin-block-end: 0.1em;
+  }
+
+  #subtitle {
+    font-style: normal;
+    font-weight: 300;
+    font-size: 24px;
+    line-height: 37px;
+    margin-block-start: 0.1em;
+    margin-block-end: 0.1em;
+
+    color: #e6e6e6;
+  }
+
+  #description {
+    font-size: 16px;
+  }
+`;
+const GitHubLogo = styled(githublogo)`
+  float: right;
+  width: 15px;
+  height: 15px;
+  border: 0;
+  position: absolute;
+  left: 45px;
+  fill: #ffffff;
+`;
+
+export default BoxCard;
--- src/data/projects.js
+++ src/data/projects.js
@@ -4,7 +4,7 @@ export default [
     subtitle: "Course API for NYU",
     description:
       "Schedge is an open source API to NYU's course catalog, geared at eventually helping NYU students plan their courses easier and faster.Our goal with this API is to make it easier for students to plan out their schedules, and eventually to also do some of that for them",
-    link: "github.com/BUGS-NYU/schedge",
+    link: "https://github.com/BUGS-NYU/schedge",
     urlLink: "https://schedge.a1liu.com/",
   },
   {
@@ -12,7 +12,7 @@ export default [
     subtitle: "Website For The Club",
     description:
       "The website is built from scratch, using React, to be a one-stop-shop for information about the OpenSource Club. It provides information about what the club is for, the projects we work on, and upcoming events. We hope to constantly update the website with features",
-    link: "github.com/BUGS-NYU/bugs-nyu.github.io",
+    link: "https://github.com/BUGS-NYU/bugs-nyu.github.io",
     urlLink: "https://bugs-nyu.github.io/",
   },
   {
@@ -20,7 +20,7 @@ export default [
     subtitle: "Mentorship Program",
     description:
       "Spark is a mentorship program run by TorchNYU, an organization dedicated towards improving NYU CS, and BUGS, NYU's open source development club. We teach students practical and pragmatic software development through working on and shipping projects.",
-    link: "github.com/torchnyu/spark",
+    link: "https://github.com/torchnyu/spark",
     urlLink: "https://spark.torchnyu.com/",
   },
   {
@@ -28,6 +28,6 @@ export default [
     subtitle: "NYU Data Hub",
     description:
       "The data hub is an open source one stop shop for course API at NYU, collected using Schedge API. The data hub contains course API since fall 2019 to current semester. In the future, we would like to extend the project into a website with helpful advice about CS classes at NYU",
-    urlLink: "github.com/BUGS-NYU/bugs-data-hub",
+    urlLink: "https://github.com/BUGS-NYU/bugs-data-hub",
   },
 ];
