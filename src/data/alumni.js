19d8f9547e36da562d54233f05a6a420e1a3c3d9
Khanh Nguyen <khanhnguyen99hn@gmail.com>
2021-02-24 10:56:12 -0500

    Update beginning

--- src/data/alumni.js
+++ src/data/alumni.js
@@ -17,41 +17,41 @@ export default {
   "Veena Arvind": {
     role: "President",
     description:
-      "Junior studying Computer Science in CAS. She loves to talk about code with other people. In her free time, Veena goes to the gym, makes jewelry, and plays card games.",
+      "Alumni who studied Computer Science in CAS. She loves to talk about code with other people. In her free time, Veena goes to the gym, makes jewelry, and plays card games.",
   },
   "Shelly Cao": {
     role: "Director of Outreach",
     description:
-      "Shelly is a Junior majoring in Computer Science with minors in Math and Business Study. She comes from a small city in China called Jingzhou and even her Chinese friends are clueless as to where it is. In her free time, she loves to explore the different boroughs in the city and try food from different cultures.",
+      "Alumni who majored in Computer Science with minors in Math and Business Study. She comes from a small city in China called Jingzhou and even her Chinese friends are clueless as to where it is. In her free time, she loves to explore the different boroughs in the city and try food from different cultures.",
   },
   "Julie Cestaro": {
     role: "",
     description:
-      "Alumni from Connecticut majoring in Computer Science and minoring in Business Studies and Math. You may also find her on campus doing work for CAS Student Council.",
+      "Alumni from Connecticut who majored in Computer Science and minored in Business Studies and Math. You may also find her on campus doing work for CAS Student Council.",
   },
   "George Ma": {
     role: "",
     description:
-      "Senior from Singapore majoring in Computer Science and Business. He likes reading, writing, coding, and gaming.",
+      "Alumni from Singapore who majored in Computer Science and Business. He likes reading, writing, coding, and gaming.",
   },
   "Deanna Tran": {
     role: "Director of Communication",
     description:
-      "Senior in CAS studying Computer Science with minors in Web Design and Linguistics. Outside of school, Deanna can be found at a local restaurant or art museum.",
+      "Alumni from CAS who studied Computer Science with minors in Web Design and Linguistics. Outside of school, Deanna can be found at a local restaurant or art museum.",
   },
   "Candice Shen": {
     role: "Marketing",
     description:
-      "Candice is a Senior at Steinhardt majoring in Media, Culture, and Communication and minoring in Web Design. If you cannot find her outside of class, Candice is traveling or planning for her 23rd country to visit.",
+      "Alumni from Steinhardt who majored in Media, Culture, and Communication and minored in Web Design. If you cannot find her outside of class, Candice is traveling or planning for her 23rd country to visit.",
   },
   "Bradley Brecher": {
     role: "",
     description:
-      "Alumni from Woodmere, New York studying Computer Science. Outside of BUGS, you can find him coaching hockey at a local high school.",
+      "Alumni from Woodmere, New York who studied Computer Science. Outside of BUGS, you can find him coaching hockey at a local high school.",
   },
   "Ami Nachiappan": {
     role: "Event Coordinator",
     description:
-      "Ami is a Senior from California studying Computer Science and minoring in Web Design. In her free time, she loves spending time with family, running, and of course, coding!",
+      "Alumni from California who studied Computer Science and minored in Web Design. In her free time, she loves spending time with family, running, and of course, coding!",
   },
 };
