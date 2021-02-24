b7c4eb1a8eee528760cc127a0a5eb824c7e27050
Yuchen <yl6448@nyu.edu>
2021-02-23 21:50:10 -0800

    [WIP] Update New Team Members (#198)

--- src/data/team.js
+++ src/data/team.js
@@ -1,17 +1,17 @@
 export default [
   {
-    name: "Albert Liu",
-    image: "aliu",
+    name: "Khanh Nguyen",
+    image: "khanhng",
     role: "President",
     description:
-      "Albert majors in Computer Science at CAS. In the past, he's worked on Schedge, an API for NYU course data, as well as HumanToHuman, an application to help researchers study human interaction patterns. In his free time he enjoys going on long walks and taking longer naps.",
+      "Khanh is a Junior from Vietnam studying Computer Science. BROCKHAMPTON is his all-time favorite boyband. Outside of school, he enjoys trying out good food, exploring the city and contributing to projects.",
   },
   {
-    name: "Samuel Lin",
-    image: "slin",
+    name: "Albert Liu",
+    image: "aliu",
     role: "Vice President",
     description:
-      "Samuel is a Senior at CAS majoring in Computer Science and minoring in Web Design and Game Design. He was born and raised in Taiwan, but studied in Singapore for high school before coming to New York. He is a big foodie and enjoys trying foods from different cultures.",
+      "Albert majors in Computer Science at CAS. In the past, he's worked on Schedge, an API for NYU course data, as well as HumanToHuman, an application to help researchers study human interaction patterns. In his free time he enjoys going on long walks and taking longer naps.",
   },
   {
     name: "Aaditya Mehta",
@@ -19,6 +19,13 @@ export default [
     role: "Treasurer",
     description:
       "Aaditya is a Senior studying Economics and Mathematics, Aaditya spends way too much time in Stern (#IAmStern). In his free time he likes to listen to music, take long walks, and read Econ papers, sometimes simultaneously.",
+  },
+  {
+    name: "Laurel Xiang",
+    image: "lxiang",
+    role: "Treasurer",
+    description:
+      "Laurel is a joint Computer Science and Data Science major at CAS. In her free time, she enjoys biking around the city, trying new foods, and contributing to projects.",
   },
   {
     name: "Jiayue(Joanna) Zhang",
@@ -35,6 +42,13 @@ export default [
       "Edmond is a Senior at CAS studying Computer Science and minoring in Business Studies. Aside from class, he likes cars, being a foodie and traveling.",
   },
   {
+    name: "Sonia Jain",
+    image: "sjain",
+    role: "Director of Marketing & Communications",
+    description:
+      "I'm Sonia and I'm a junior at New York University double majoring in Computer Science and Media, Culture, and Communications. I am interested in game design, art, and I love animals. In my free time, I work with a variety of organizations to help find dogs and cats' homes. I also love painting and digital art so when I'm not working with animals, I am creating.",
+  },
+  {
     name: "Eric He",
     image: "eriche",
     role: "Secretary",
@@ -42,17 +56,38 @@ export default [
       "Eric is a Senior studying Computer Science and Mathematics. He likes to play basketball, listens to music and read about philosophy.",
   },
   {
-    name: "Khanh Nguyen",
-    image: "khanhng",
-    role: "Webmaster",
+    name: "Bruce Jiang",
+    image: "bjiang",
+    role: "Secretary",
     description:
-      "Khanh is a Junior from Vietnam studying Computer Science. BROCKHAMPTON is his all-time favorite boyband. Outside of school, he enjoys trying out good food, exploring the city and contributing to projects.",
+      "Bruce is a junior from China studying Computer Science and Mathematics. Outside of class, he is crazy in soccer (especially in Chelsea) and basketball. He spends tons of time playing FIFA (most of the time playing Chelsea). Besides, he likes listening to music, reading about history, and travelling.",
   },
   {
-    name: "Ami Nachiappan",
-    image: "anachiappan",
+    name: "Evan Silverman",
+    image: "esilverman",
+    role: "Webmaster",
+    description:
+      "Evan is a Junior studying CS at CAS. In his free time he enjoys working on too many side projects, expanding his record collection, and making memes for @nyumsfbt",
+  },
+  {
+    name: "Yuchen Liu",
+    image: "yliu",
+    role: "Webmaster",
+    description:
+      "Yuchen is a CS and Data Science major at CAS. He likes to eat ramen, watch movies, meet new people, and work on fun projects.",
+  },
+  {
+    name: "Samuel Lin",
+    image: "slin",
     role: "Event Coordinator",
     description:
-      "Ami is a Senior from California studying Computer Science and minoring in Web Design. In her free time, she loves spending time with family, running, and of course, coding!",
+      "Samuel is a Senior at CAS majoring in Computer Science and minoring in Web Design and Game Design. He was born and raised in Taiwan, but studied in Singapore for high school before coming to New York. He is a big foodie and enjoys trying foods from different cultures.",
+  },
+  {
+    name: "Vishal Vanam",
+    image: "vvanam",
+    role: "Event Coordinator",
+    description:
+      "I am a Graduate Student in Information Systems at GSAS. I hail from Hyderabad(India) and love travelling and exploring new places. Usually spotted listening to music and engaging in debates with friends.",
   },
 ];
