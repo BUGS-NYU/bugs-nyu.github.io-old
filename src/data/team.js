c6bd531812b0de4ac45394cc0f1652fbba5263b9
Ajinkya-Sonawane <sonawaneajinks@gmail.com>
2021-06-13 22:46:00 +0530

    Update Eboard data #227

--- src/data/team.js
+++ src/data/team.js
@@ -7,13 +7,6 @@ export default [
       "Khanh is a Junior from Vietnam studying Computer Science. BROCKHAMPTON is his all-time favorite boyband. Outside of school, he enjoys trying out good food, exploring the city and contributing to projects.",
   },
   {
-    name: "Albert Liu",
-    image: "aliu",
-    role: "Vice President",
-    description:
-      "Albert majors in Computer Science at CAS. In the past, he's worked on Schedge, an API for NYU course data, as well as HumanToHuman, an application to help researchers study human interaction patterns. In his free time he enjoys going on long walks and taking longer naps.",
-  },
-  {
     name: "Aaditya Mehta",
     image: "amehta",
     role: "Treasurer",
@@ -28,32 +21,11 @@ export default [
       "Laurel is a joint Computer Science and Data Science major at CAS. In her free time, she enjoys biking around the city, trying new foods, and contributing to projects.",
   },
   {
-    name: "Jiayue(Joanna) Zhang",
-    image: "joannazh",
-    role: "Director of Marketing",
-    description:
-      "Senior in CAS majoring in Mathematics and Computer Science. She enjoys working on machine learning research, supporting local restaurants, capturing NYC through her lens, and taking a walk at sunset.",
-  },
-  {
-    name: "Edmond Shao",
-    image: "eshao",
-    role: "Director of Communications",
-    description:
-      "Edmond is a Senior at CAS studying Computer Science and minoring in Business Studies. Aside from class, he likes cars, being a foodie and traveling.",
-  },
-  {
     name: "Sonia Jain",
     image: "sjain",
     role: "Director of Marketing & Communications",
     description:
       "I'm Sonia and I'm a junior at New York University double majoring in Computer Science and Media, Culture, and Communications. I am interested in game design, art, and I love animals. In my free time, I work with a variety of organizations to help find dogs and cats' homes. I also love painting and digital art so when I'm not working with animals, I am creating.",
-  },
-  {
-    name: "Eric He",
-    image: "eriche",
-    role: "Secretary",
-    description:
-      "Eric is a Senior studying Computer Science and Mathematics. He likes to play basketball, listens to music and read about philosophy.",
   },
   {
     name: "Bruce Jiang",
